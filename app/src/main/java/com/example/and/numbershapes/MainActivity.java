6876e920c9d6fafa04f7ed0ec6044bc881fb8f7b
Andressa <andressacsferreira@gmail.com>
2020-07-23 22:29:48 +0100

    corrected empty form

--- app/src/main/java/com/example/and/numbershapes/MainActivity.java
+++ app/src/main/java/com/example/and/numbershapes/MainActivity.java
@@ -16,41 +16,61 @@ public class MainActivity extends AppCompatActivity {
         int value;
 
         public boolean isTriangular(){
+
+            // if N*8+1 is a square number, then N is a triangular number
             double sq = Math.sqrt(value*8+1);
+
+            // math.floor get the integer part from a double
             return (sq - Math.floor(sq)) == 0;
+
             // https://en.wikipedia.org/wiki/Square_triangular_number
         }
 
         public boolean isSquared(){
+
+            // if the square root is integer the number is a square number
             double sq = Math.sqrt(value);
             return (sq - Math.floor(sq)) == 0;
         }
     }
 
     public void checkNumber(View view){
+
+        // check if button was pressed
         Log.i("info", "button pressed");
-        EditText number=(EditText) findViewById(R.id.editText2);
-        int numberInt= Integer.parseInt(number.getText().toString());
-        Number testNumber= new Number();
-        testNumber.value=numberInt;
         String message;
 
-        if (testNumber.isSquared() && testNumber.isTriangular()){
-            message=numberInt+" is a square and a triangular number at the same time.";
-        } else if (testNumber.isSquared()){
-            message=numberInt+" is a square number.";
-        } else if (testNumber.isTriangular()){
-            message=numberInt+" is a triangular number.";
+        // take number from the form
+        EditText number=(EditText) findViewById(R.id.editText2);
+        if (number.getText().toString().isEmpty()){
+            message="Please type a number.";
         } else {
-            message=numberInt+" is not triangular or squared.";
-        }
+            int numberInt = Integer.parseInt(number.getText().toString());
 
+            // use the class Number
+            Number testNumber = new Number();
+            testNumber.value = numberInt;
+
+            // prepare to print message
+            message = number.getText().toString();
+
+            if (testNumber.isSquared() && testNumber.isTriangular()) {
+                message += " is a square and a triangular number.";
+            } else if (testNumber.isSquared()) {
+                message += " is a square number.";
+            } else if (testNumber.isTriangular()) {
+                message += " is a triangular number.";
+            } else {
+                message += " is neither triangular nor squared.";
+            }
+        }
+        // show a floating message that desapear after a few seconds
         Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
+        // clean the form
         number.setText("");
 
     }
-    //n(n+1)/2
-    //n^2
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
